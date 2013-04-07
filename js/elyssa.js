044e36ab2b7d528976a809445345e9143ca4341c
Johannes Stein <johannesstein@freeze-dev.com>
2013-04-07 22:45:22 +0200

    Updated JS

--- js/elyssa.js
+++ js/elyssa.js
@@ -399,6 +399,16 @@
 
 (function() {
 
+  define('elyssa', [], function() {
+    if (!(typeof module !== "undefined" && module !== null ? module.exports : void 0)) {
+      return window.Elyssa = {};
+    }
+  });
+
+}).call(this);
+
+(function() {
+
   (function(window, Elyssa) {
     'use strict';    return Elyssa.Log = {};
   })(this, this.Elyssa || (this.Elyssa = {}));
