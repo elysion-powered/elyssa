8fe46fc17618f9c738f648c8c1708f35bf9b63cd
Johannes Stein <johannesstein@freeze-dev.com>
2013-07-30 22:04:28 +0200

    Updated dist JS

--- dist/elyssa.js
+++ dist/elyssa.js
@@ -142,56 +142,57 @@
 }).call(this);
 
 (function() {
-  (function() {
-    var vendors;
-    vendors = ['ms', 'moz', 'webkit', 'o'];
-    define('requestAnimationFrame', ['root'], function(root) {
-      var lastTime, requestAnimationFrame, x, _i, _len;
+  var vendors;
+
+  vendors = ['ms', 'moz', 'webkit', 'o'];
+
+  define('requestanimationframe', ['root'], function(root) {
+    var lastTime, requestAnimationFrame, x, _i, _len;
+    requestAnimationFrame = root.requestAnimationFrame;
+    if (!requestAnimationFrame) {
+      for (_i = 0, _len = vendors.length; _i < _len; _i++) {
+        x = vendors[_i];
+        requestAnimationFrame = root["" + x + "RequestAnimationFrame"];
+        if (requestAnimationFrame) {
+          break;
+        }
+      }
+    }
+    if (!requestAnimationFrame) {
       lastTime = 0;
-      requestAnimationFrame = root.requestAnimationFrame;
-      if (!requestAnimationFrame) {
-        for (_i = 0, _len = vendors.length; _i < _len; _i++) {
-          x = vendors[_i];
-          requestAnimationFrame = root["" + x + "RequestAnimationFrame"];
-          if (requestAnimationFrame) {
-            break;
-          }
+      requestAnimationFrame = function(callback, element) {
+        var currTime, id, timeToCall, _ref;
+        currTime = (_ref = performance.now()) != null ? _ref : Date.now();
+        timeToCall = Math.max(0, 16 - (currTime - lastTime));
+        id = root.setTimeout((function() {
+          return callback(currTime + timeToCall);
+        }), timeToCall);
+        lastTime = currTime + timeToCall;
+        return id;
+      };
+    }
+    return requestAnimationFrame;
+  });
+
+  define('cancelanimationframe', ['root'], function(root) {
+    var cancelAnimationFrame, x, _i, _len;
+    cancelAnimationFrame = root.cancelAnimationFrame;
+    if (!cancelAnimationFrame) {
+      for (_i = 0, _len = vendors.length; _i < _len; _i++) {
+        x = vendors[_i];
+        cancelAnimationFrame = root["" + x + "CancelAnimationFrame"] || root["" + x + "CancelRequestAnimationFrame"];
+        if (cancelAnimationFrame) {
+          break;
         }
       }
-      if (!requestAnimationFrame) {
-        requestAnimationFrame = function(callback, element) {
-          var currTime, id, timeToCall;
-          currTime = Date.now();
-          timeToCall = Math.max(0, 16 - (currTime - lastTime));
-          id = root.setTimeout((function() {
-            return callback(currTime + timeToCall);
-          }), timeToCall);
-          lastTime = currTime + timeToCall;
-          return id;
-        };
-      }
-      return requestAnimationFrame;
-    });
-    return define('cancelAnimationFrame', ['root'], function(root) {
-      var cancelAnimationFrame, x, _i, _len;
-      cancelAnimationFrame = root.cancelAnimationFrame;
-      if (!cancelAnimationFrame) {
-        for (_i = 0, _len = vendors.length; _i < _len; _i++) {
-          x = vendors[_i];
-          cancelAnimationFrame = root["" + x + "CancelAnimationFrame"] || root["" + x + "CancelRequestAnimationFrame"];
-          if (cancelAnimationFrame) {
-            break;
-          }
-        }
-      }
-      if (!cancelAnimationFrame) {
-        cancelAnimationFrame = function(id) {
-          return root.clearTimeout(id);
-        };
-      }
-      return cancelAnimationFrame;
-    });
-  })();
+    }
+    if (!cancelAnimationFrame) {
+      cancelAnimationFrame = function(id) {
+        return root.clearTimeout(id);
+      };
+    }
+    return cancelAnimationFrame;
+  });
 
 }).call(this);
 
@@ -466,21 +467,37 @@
       }
 
       EventMap.prototype.serialize = function() {
-        return JSON.stringify(this.events, function(key, value) {
-          if (typeof value === 'function') {
-            value = value.toString();
-          }
-          return value;
-        });
+        var err, result;
+        try {
+          result = JSON.stringify(this.events, function(key, value) {
+            if (typeof value === 'function') {
+              value = value.toString();
+            }
+            return value;
+          });
+        } catch (_error) {
+          err = _error;
+          console.error("Error while serializing eventmap: " + err);
+        }
+        return result;
       };
 
       EventMap.prototype.deserialize = function(string) {
-        return this.events = JSON.parse(string, function(key, value) {
-          if (value.indexOf('function') === 0) {
-            value = new Function(value)();
-          }
-          return value;
-        });
+        var err, events;
+        try {
+          events = JSON.parse(string, function(key, value) {
+            if (value.indexOf('function') === 0) {
+              value = new Function(value)();
+            }
+            return value;
+          });
+        } catch (_error) {
+          err = _error;
+          console.error("Error while deserializing eventmap: " + err);
+          return false;
+        }
+        this.events = events;
+        return true;
       };
 
       EventMap.prototype.on = function(eventName, eventFunction) {
